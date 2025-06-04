2adf56fd4977d91a892f0219f69d9e1e193a0954
Akshay Jadhav <123619632+AkshayJ9@users.noreply.github.com>
2025-06-04 13:09:49 +0530

    Update utils.js

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,3 +1,4 @@
 // export const BACKEND_URL = "https://courseapp-73af.onrender.com/api/v1";
 
-export const BACKEND_URL = "http://localhost:4001/api/v1";
+export const BACKEND_URL = "https://e-commerce-course-app.onrender.com/api/v1";
+
